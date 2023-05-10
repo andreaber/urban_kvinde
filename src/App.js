eea59ef31ceac275daf294954309c182a8c526ec
Andrea Berardi <andrea.berardi@hotmail.com>
2023-05-10 11:22:31 -0300

    Creación de la app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Andrea Berardi
         </p>
         <a
           className="App-link"
